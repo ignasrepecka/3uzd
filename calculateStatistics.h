b9a169809b6f4ff474afa64110c9557556849245
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-04 01:46:54 +0200

    Update calculateStatistics.h

--- calculateStatistics.h
+++ calculateStatistics.h
@@ -3,16 +3,43 @@
 
 #include <vector>
 #include <string>
+#include <istream>
 
-struct Studentas {
+class Studentas {
+    private:
     std::string vardas;
     std::string pavarde;
     std::vector<int> balai;
     double vidurkis;
     double mediana;
     int egz;
-};
 
-void calculateStatistics(Studentas& studentas);
+public:
+    // Constructor
+    Studentas() : vidurkis(0), mediana(0), egz(0) {}
+
+    // Getters
+    inline std::string getVardas() const { return vardas; }
+    inline std::string getPavarde() const { return pavarde; }
+    std::vector<int> getBalai() const { return balai; }
+    double getVidurkis() const { return vidurkis; }
+    double getMediana() const { return mediana; }
+    int getEgz() const { return egz; }
+
+    // Other member functions
+    void calculateStatistics();
+
+    // New method
+    std::istream& readStudent(std::istream& is) {
+        is >> this->vardas >> this->pavarde;
+        for(int i = 0; i < 3; i++) {
+            int j;
+            is >> j;
+            this->balai.push_back(j);
+        }
+        is >> this->egz;
+        return is;
+    }
+};
 
 #endif // CALCULATESTATISTICS_H_INCLUDED
