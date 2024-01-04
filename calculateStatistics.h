9c67529d7cf76b47b46ece904e35e61aa14e623a
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-05 01:14:20 +0200

    Update calculateStatistics.h

--- calculateStatistics.h
+++ calculateStatistics.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <istream>
+#include <ostream>
 
 class Studentas {
     private:
@@ -18,7 +19,29 @@ public:
     // Constructor
     Studentas() : vidurkis(0), mediana(0), egz(0) {}
 
-    ~Studentas() {}
+    // Rule of Three
+    // 1. Destructor
+    ~Studentas() {
+        std::cout << "Destructor called for " << vardas << std::endl;
+    }
+
+    // 2. Copy constructor
+    Studentas(const Studentas& other)
+        : vardas(other.vardas), pavarde(other.pavarde), balai(other.balai),
+          vidurkis(other.vidurkis), mediana(other.mediana), egz(other.egz) {}
+
+    // 3. Copy assignment operator
+    Studentas& operator=(const Studentas& other) {
+        if (this != &other) {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            balai = other.balai;
+            vidurkis = other.vidurkis;
+            mediana = other.mediana;
+            egz = other.egz;
+        }
+        return *this;
+    }
 
     // Getters
     inline std::string getVardas() const { return vardas; }
@@ -41,6 +64,20 @@ public:
         }
         is >> this->egz;
         return is;
+    }
+
+    // Input/output operators
+    friend std::istream& operator>>(std::istream& is, Studentas& s) {
+        return s.readStudent(is);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& s) {
+        os << s.getVardas() << " " << s.getPavarde() << " Grades: ";
+        for (int grade : s.getBalai()) {
+            os << grade << " ";
+        }
+        os << "Exam: " << s.getEgz();
+        return os;
     }
 };
 
