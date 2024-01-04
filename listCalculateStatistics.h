269fa860e2626d83859dfe0795cb8be629c4fb88
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-04 19:31:58 +0200

    Update listCalculateStatistics.h

--- listCalculateStatistics.h
+++ listCalculateStatistics.h
@@ -3,16 +3,43 @@
 
 #include <list>
 #include <string>
+#include <istream>
 
-struct Studentasl {
+class Studentasl {
+    private:
     std::string vardas;
     std::string pavarde;
     std::list<int> balai;
     double vidurkis;
     double mediana;
     int egz;
-};
 
-void listCalculateStatistics(Studentasl& studentasl);
+public:
+    // Constructor
+    Studentasl() : vidurkis(0), mediana(0), egz(0) {}
+
+    // Getters
+    inline std::string getVardas() const { return vardas; }
+    inline std::string getPavarde() const { return pavarde; }
+    std::list<int> getBalai() const { return balai; }
+    double getVidurkis() const { return vidurkis; }
+    double getMediana() const { return mediana; }
+    int getEgz() const { return egz; }
+
+    // Other member functions
+    void listCalculateStatistics();
+
+    // New method
+    std::istream& readStudent(std::istream& is) {
+        is >> this->vardas >> this->pavarde;
+        for(int i = 0; i < 3; i++) {
+            int j;
+            is >> j;
+            this->balai.push_back(j);
+        }
+        is >> this->egz;
+        return is;
+    }
+};
 
 #endif // LISTCALCULATESTATISTICS_H_INCLUDED
