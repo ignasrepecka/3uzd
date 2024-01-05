8ede07f884fbf809536b5a8ecfedd90da404671d
ignasrepecka <146369153+ignasrepecka@users.noreply.github.com>
2024-01-05 05:32:27 +0200

    Update calculateStatistics.cpp

--- calculateStatistics.cpp
+++ calculateStatistics.cpp
@@ -14,8 +14,10 @@ void Studentas::calculateStatistics() {
     for(int j : this->getBalai()) {
         suma += j;
     }
+    suma += this->getEgz(); // Include the exam grade in the sum
 
     std::vector<int> balaiCopy = this->getBalai();
+    balaiCopy.push_back(this->getEgz()); // Include the exam grade in the grades vector
     sort(balaiCopy.begin(), balaiCopy.end());
     if ((balaiCopy.size()) % 2 == 0)
         this->mediana = (balaiCopy[balaiCopy.size()/2 - 1] + balaiCopy[balaiCopy.size()/2]) / 2.0;
@@ -23,4 +25,10 @@ void Studentas::calculateStatistics() {
         this->mediana = balaiCopy[balaiCopy.size()/2];
 
     this->vidurkis = suma/balaiCopy.size();
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Studentas& s) {
+    os << s.getVardas() << " " << s.getPavarde() << " " << s.getVidurkis();
+    return os;
 }
